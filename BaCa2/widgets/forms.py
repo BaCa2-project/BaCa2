3e37c9898d60d8748937daed69dd5dea90ff9ed5
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2023-08-10 23:02:13 +0200

    Form FRNT-5 [IN PROGRESS] -Form templates FRNT-48 [IN PROGRESS] -FormWidget auxiliary class FRNT-49 [IN PROGRESS] Login Screen FRNT-9 [IN PROGRESS] -Login template FRNT-50 [IN PROGRESS]

--- BaCa2/widgets/forms.py
+++ BaCa2/widgets/forms.py
@@ -3,15 +3,18 @@ class FormWidget:
     def __init__(self,
                  button_text: str = 'Submit',
                  display_non_field_validation: bool = True,
-                 display_field_errors: bool = True) -> None:
+                 display_field_errors: bool = True,
+                 floating_labels: bool = True,) -> None:
         self.button_text = button_text
         self.display_non_field_validation = display_non_field_validation
         self.display_field_errors = display_field_errors
+        self.floating_labels = floating_labels
 
     def get_context(self) -> dict:
         context = {
             'button_text': self.button_text,
             'display_non_field_errors': self.display_non_field_validation,
             'display_field_errors': self.display_field_errors,
+            'floating_labels': self.floating_labels,
         }
         return context
