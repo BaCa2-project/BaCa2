24150dbdc895342e9e5a39fb9f41b197faa47fe4
Bartosz Deptuła <barti.deptula@gmail.com>
2023-08-17 12:57:26 +0200

    Environment changes for django

---
+++ BaCa2/BaCa2/db/ext_databases.py
@@ -0,0 +1,10 @@
+
+DATABASES['test_simple_course'] = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'test_simple_course_db',
+    'USER': 'baca2',
+    'PASSWORD': 'zaqwsxcde',
+    'HOST': 'localhost',
+    'PORT': ''
+}
+
