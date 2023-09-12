82c3bd4c50b652d55cba70b086eed236313e2f8e
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2023-09-12 16:46:47 +0200

    WIDGETS FRNT-2 [IN PROGRESS]
    Form FRNT-5 [IN PROGRESS]
    - Form template(s) FRNT-48 [IN PROGRESS] : fixed issues related to the functioning and appearance of toggleable fields in cases of field validation errors

--- BaCa2/assets/js/forms.js
+++ BaCa2/assets/js/forms.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 });
 
 function toggleField(field_name, button_text_off, button_text_on) {
+    console.log(field_name);
     const field = document.getElementById(field_name);
     const toggleable = field.closest('.toggleable');
     const button = $(toggleable).find('button');
@@ -18,6 +19,12 @@ function toggleField(field_name, button_text_off, button_text_on) {
         $(field).attr('disabled', false);
         button.text(button_text_on);
     } else {
+        if (field.classList.contains('is-invalid')) {
+            field.classList.remove('is-invalid');
+            const errors = $(field.closest('.input-block')).find('.invalid-feedback');
+            console.log(errors);
+            errors.remove();
+        }
         toggleable.classList.remove('toggleable-on');
         toggleable.classList.add('toggleable-off');
         $(field).attr('disabled', true);
