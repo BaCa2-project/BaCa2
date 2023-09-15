f828c5c9feae322cfc7412770ab5f5b5099bd740
Bartosz Deptuła <barti.deptula@gmail.com>
2023-09-15 14:58:51 +0200

    Added sample package, edited tests for broker communication

--- BaCa2/broker_api/tests.py
+++ BaCa2/broker_api/tests.py
@@ -1,21 +1,25 @@
+from datetime import datetime, timedelta
 
 import os
 import json
 from http import server
 import cgi
+from django.core.files.uploadedfile import SimpleUploadedFile
 from threading import Thread
 from pathlib import Path
 
 import requests
 from django.test import TestCase, Client
 
-from main.models import Course
+from BaCa2.settings import BACA_PASSWORD
+from baca2PackageManager import Package
+from broker_api.broker_communication import BacaToBroker, BrokerToBaca
+from broker_api.models import BrokerSubmit
 from course.manager import create_course, delete_course
-from .models import BrokerSubmit
-
-from package.models import PackageInstance, PackageSource
-# from baca2PackageManager.broker_communication import *
-from .broker_communication import *  # TODO: ^ replace with the above
+from course.models import Round, Task, Submit
+from course.routing import InCourse
+from main.models import Course, User
+from package.models import PackageSource, PackageInstance
 
 
 class BacaApiHandler(server.BaseHTTPRequestHandler):
@@ -44,37 +48,56 @@ class BacaApiHandler(server.BaseHTTPRequestHandler):
 
 class General(TestCase):
 
-    def setUp(self) -> None:
-        self.server = server.HTTPServer(('127.0.0.1', 8180), BacaApiHandler)
-        self.thread = Thread(target=self.server.serve_forever)
-        self.thread.start()
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.server = server.HTTPServer(('127.0.0.1', 8180), BacaApiHandler)
+        cls.thread = Thread(target=cls.server.serve_forever)
+        cls.thread.start()
 
-        self.course = Course(
-            name='course1',
-            short_name='c1',
-            db_name='course1_db'
-        )
-        self.course.save()
-        create_course(self.course.name)
-        self.source = PackageSource(
-            name='kolejka'
-        )
-        self.source.save()
-        self.package = PackageInstance(
-            package_source=self.source,
-            commit=12
-        )
-        self.package.save()
+        cls.course = Course(name='course1', short_name='c1', db_name='course1_db')
+        cls.course.save()
+        create_course(cls.course.name)
 
-    def tearDown(self) -> None:
-        self.server.shutdown()
-        self.server.server_close()
-        self.thread.join()
+        package = PackageSource.objects.create(name='dosko')
+        cls.pkg_instance = PackageInstance.objects.create(package_source=package, commit='1')
+        package.save()
+        cls.pkg_instance.save()
+
+        cls.user = User.objects.create_user(username='user1', password='user1', email='test@test.pl')
+
+        with InCourse(cls.course.name):
+            round_ = Round.objects.create(start_date=datetime.now(),
+                                          deadline_date=datetime.now() + timedelta(days=1),
+                                          reveal_date=datetime.now() + timedelta(days=2))
+            round_.save()
+
+            cls.task = Task.create_new(
+                task_name="Liczby doskonałe",
+                package_instance=cls.pkg_instance,
+                round=round_,
+                points=10,
+            )
+            cls.task.save()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.server.shutdown()
+        cls.server.server_close()
+        cls.thread.join()
 
         delete_course(self.course.name)
 
     def test_basic(self):
-        submit = BrokerSubmit.send(self.course, 1, self.package)
+        src_code = SimpleUploadedFile(name='src.cpp',
+                                      content=b'#include<iostream>\nint main(){ std::cout << 1; }',
+                                      content_type='text/plain')
+        submit_id = None
+        with InCourse(self.course.name):
+            submit = Submit.create_new(source_code=src_code, task=self.task, usr=self.user)
+            submit.save()
+            submit_id = submit.pk
+
+        broker_submit = BrokerSubmit.send(self.course, submit_id, self.pkg_instance)
         c = Client()
         r = c.post(path='http://127.0.0.1:8000/broker_api/result/course1/1',
                    data={
