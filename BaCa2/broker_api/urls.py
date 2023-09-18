cdbef9ac6cf6290666cc57a6b7caf6fb15ff6d90
ZyndramZM <barti.deptula@gmail.com>
2023-09-18 23:35:25 +0200

    BRK-6 Broker communication update after merge.

--- BaCa2/broker_api/urls.py
+++ BaCa2/broker_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("result/<str:submit_id>", views.handle_broker_result, name='brokerApiResult'),
+    path("result/<str:broker_submit_id>", views.handle_broker_result, name='brokerApiResult'),
     # path("status/<str:submit_id>", views.handle_broker_status, name='brokerApiStatus')
 ]
