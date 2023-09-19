191dee8008a1c76c33838ae9696312dae7ff91ef
ZyndramZM <barti.deptula@gmail.com>
2023-09-19 15:17:07 +0200

    BRK-6 Full communication BaCa2->broker->KOLEJKA->broker->BaCa2 works.

--- BaCa2/broker_api/tests.py
+++ BaCa2/broker_api/tests.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 import json
 from http import server
 import cgi
+from time import sleep
+
 from django.core.files.uploadedfile import SimpleUploadedFile
 from threading import Thread
 
@@ -16,6 +18,7 @@ from course.models import Round, Task, Submit
 from course.routing import InCourse
 from main.models import Course, User
 from package.models import PackageSource, PackageInstance
+from BaCa2.settings import SUBMITS_DIR
 
 
 class BacaApiHandler(server.BaseHTTPRequestHandler):
@@ -53,10 +56,6 @@ class General(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.server = server.HTTPServer(('127.0.0.1', 8180), BacaApiHandler)
-        cls.thread = Thread(target=cls.server.serve_forever)
-        cls.thread.start()
-
         cls.course = Course(name='course1', short_name='c1', db_name='course1_db')
         cls.course.save()
         create_course(cls.course.name)
@@ -66,7 +65,9 @@ class General(TestCase):
         package.save()
         cls.pkg_instance.save()
 
-        cls.user = User.objects.create_user(username='user1', password='user1', email='test@test.pl')
+        cls.user = User.objects.create_user(username=f'user1_{datetime.now().timestamp()}',
+                                            password='user1',
+                                            email=f'test{datetime.now().timestamp()}@test.pl')
 
         with InCourse(cls.course.name):
             round_ = Round.objects.create(start_date=datetime.now(),
@@ -84,31 +85,55 @@ class General(TestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
-        cls.server.shutdown()
-        cls.server.server_close()
-        cls.thread.join()
-
         delete_course(cls.course.name)
 
-    def test_basic(self):
-        src_code = SimpleUploadedFile(name='src.cpp',
-                                      content=b'#include<iostream>\nint main(){ std::cout << 1; }',
-                                      content_type='text/plain')
+    def tearDown(self) -> None:
+        if self.server is not None:
+            self.server.shutdown()
+            self.server.server_close()
+            self.thread.join()
+            self.server = None
+            self.thread = None
+
+    # def test_basic(self):
+    #     self.server = server.HTTPServer(('127.0.0.1', 8180), BacaApiHandler)
+    #     self.thread = Thread(target=self.server.serve_forever)
+    #     self.thread.start()
+    #
+    #     src_code = SUBMITS_DIR / '1234.cpp'
+    #
+    #     submit_id = None
+    #     with InCourse(self.course.name):
+    #         submit = Submit.create_new(source_code=src_code, task=self.task, usr=self.user)
+    #         submit.save()
+    #         submit_id = submit.pk
+    #
+    #     broker_submit = BrokerSubmit.send(self.course, submit_id, self.pkg_instance)
+    #     c = Client()
+    #     data = BrokerToBaca(
+    #         pass_hash=broker_submit.hash_password(BACA_PASSWORD),
+    #         submit_id=broker_submit.broker_id,
+    #         results={}
+    #     )
+    #     r = c.post(path=f'http://127.0.0.1:8000/broker_api/result/{broker_submit.broker_id}',
+    #                data=data.serialize(),
+    #                content_type='application/json')
+    #     broker_submit.refresh_from_db()
+    #     self.assertTrue(broker_submit.status == broker_submit.StatusEnum.CHECKED)
+
+    def test_broker_communication(self):
+        # this test requires running broker server on localhost:8180
+
+        src_code = SUBMITS_DIR / '1234.cpp'
+        src_code = src_code.absolute()
+
         submit_id = None
         with InCourse(self.course.name):
             submit = Submit.create_new(source_code=src_code, task=self.task, usr=self.user)
+            submit.pk = datetime.now().timestamp()
             submit.save()
             submit_id = submit.pk
 
         broker_submit = BrokerSubmit.send(self.course, submit_id, self.pkg_instance)
-        c = Client()
-        data = BrokerToBaca(
-            pass_hash=broker_submit.hash_password(BACA_PASSWORD),
-            submit_id=broker_submit.broker_id,
-            results={}
-        )
-        r = c.post(path=f'http://127.0.0.1:8000/broker_api/result/{broker_submit.broker_id}',
-                   data=data.serialize(),
-                   content_type='application/json')
-        broker_submit.refresh_from_db()
-        self.assertTrue(broker_submit.status == broker_submit.StatusEnum.CHECKED)
+
+        sleep(300)
