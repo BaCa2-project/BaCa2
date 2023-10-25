62500474804e81ee2cae9a6355803ef8d08578be
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2023-10-25 20:02:05 +0200

    course management and authentication refactor testing
    COURSE MAIN-19 [IN PROGRESS]
    Course Model MAIN-8 [AWAITING TESTING] -> [IN TESTING]
    Course Manager MAIN-18 [AWAITING TESTING] -> [IN TESTING]
    Role Model MAIN-30 [AWAITING TESTING] -> [IN TESTING]
    Role Manager MAIN-29 [AWAITING TESTING] -> [IN TESTING]
    Role
    RolePreset Manager MAIN-32 [IN PROGRESS]

---
+++ BaCa2/BaCa2/db/ext_databases.py
@@ -0,0 +1,10 @@
+
+DATABASES['c_2023'] = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'c_2023_db',
+    'USER': 'baca2',
+    'PASSWORD': 'zaqwsxcde',
+    'HOST': 'localhost',
+    'PORT': ''
+}
+
