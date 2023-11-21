6d88aedfb42e25542159411e2b72ee11044e446f
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2023-11-21 19:35:09 +0100

    Login page stylistic revamp.
    LOGIN SCREEN FRNT-9 [PAUSED] -> [IN PROGRESS]
    Form reveal animation FRNT-91 [OPEN] -> [DONE]

--- BaCa2/assets/js/login.js
+++ BaCa2/assets/js/login.js
@@ -1,5 +1,48 @@
-function displayLoginForm(){
-    // $('#button-wrapper').removeClass('active');
-    $("#button-wrapper").prop("disabled", true);
-    $("#form-wrapper").addClass('active');
+function transformLoginFormBtn(loginFormBtn, text, cls, loginForm) {
+    loginFormBtn.position("relative");
+    loginFormBtn.text(text);
+    loginFormBtn.attr("class", cls);
+    loginFormBtn.click(() => loginForm.submit());
+}
+
+function initLoginPage() {
+    const loginForm = $("#login_form");
+    const loginBtn = loginForm.find("button");
+    const loginFormBtn = $("#login-form-btn");
+    const alert = $(".alert-danger");
+
+    if (!sessionStorage.getItem("loginFormRefresh")) {
+        loginForm.hide();
+        loginBtn.hide();
+        loginFormBtn.css("transition", "all 0.8s ease");
+        loginFormBtn.click(() => {
+            $("#login_form").slideDown("slow");
+            transformLoginFormBtn(loginFormBtn, loginBtn.text(), loginBtn.attr("class"), loginForm);
+            shrinkLogo($(".logo-wrapper"));
+        });
+    }
+    else {
+        loginBtn.hide();
+        transformLoginFormBtn(loginFormBtn, loginBtn.text(), loginBtn.attr("class"), loginForm);
+
+        if (!sessionStorage.getItem("loginFormAlert"))
+            alert.hide();
+    }
+
+    sessionStorage.removeItem("loginFormRefresh");
+
+    $(document).on("submit", "#login_form",
+        (e) => sessionStorage.setItem("loginFormRefresh", "true"))
+}
+
+function displayLoginFormAlert() {
+    const alert = $(".alert-danger");
+
+    if (!sessionStorage.getItem("loginFormAlert"))
+            alert.slideDown("slow");
+
+    if (alert.length > 0)
+        sessionStorage.setItem("loginFormAlert", "true");
+    else
+        sessionStorage.removeItem("loginFormAlert");
 }
