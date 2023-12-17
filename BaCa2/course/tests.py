86ef4a74bb6d41264ef55fc43e3b3a0f1b7e580f
Bartosz Deptuła <barti.deptula@gmail.com>
2023-12-17 17:54:19 +0100

    Package app update: - added models managers, - added tests for package actions - added package instance user workflow - added zip unpacking into package instance / package source

--- BaCa2/course/tests.py
+++ BaCa2/course/tests.py
@@ -1,9 +1,6 @@
 from random import choice
 
-import unittest
-
 from django.core.exceptions import ValidationError
-from pathlib import Path
 from datetime import datetime, timedelta
 
 from django.test import TestCase
@@ -30,26 +27,11 @@ def create_rounds(course, amount):
     return rounds
 
 
-def create_tasks(round_, amount):
-    with InCourse(round_.course):
-        tasks = []
-        for i in range(amount):
-            new_task = Task.objects.create(
-                package_instance_id=i,
-                task_name=f'Test Task {i}',
-                round=round_,
-                points=10,
-            )
-            new_task.save()
-            tasks.append(new_task)
-    return tasks
-
-
 def create_package_task(course, round_, pkg_name, commit, init_task: bool = True):
     if not PackageInstance.objects.filter(package_source__name=pkg_name, commit=commit).exists():
         pkg = PackageInstance.objects.create_source_and_instance(pkg_name, commit)
     else:
-        pkg = PackageInstance.objects.get(package_name=pkg_name, commit=commit)
+        pkg = PackageInstance.objects.get(package_source__name=pkg_name, commit=commit)
     with InCourse(course):
         task = Task.objects.create_task(
             package_instance=pkg,
@@ -96,6 +78,7 @@ def create_submit(course, task, user, src_code):
 
 class RoundTest(TestCase):
     course = None
+    pkg = None
 
     @classmethod
     def setUpTestData(cls):
@@ -103,10 +86,14 @@ class RoundTest(TestCase):
             name='Test Course',
             short_name='TC1',
         )
+        cls.pkg = PackageInstance.objects.create_source_and_instance('dosko', '1')
 
     @classmethod
     def tearDownClass(cls):
         Course.objects.delete_course(cls.course)
+        pkg_src = cls.pkg.package_source
+        cls.pkg.delete()
+        pkg_src.delete()
 
     def tearDown(self):
         with InCourse(self.course):
@@ -183,49 +170,41 @@ class RoundTest(TestCase):
                 start_date=timezone.now() - timedelta(days=1),
                 deadline_date=timezone.now() + timedelta(days=1),
             )
-            self.assertTrue(round1.is_open())
+            self.assertTrue(round1.is_open)
             round1.delete()
+
+    def generic_round_with_2_tasks(self,
+                                   init_tasks=False) -> Round:
+        round_ = Round.objects.create_round(
+            start_date=timezone.now() - timedelta(days=1),
+            deadline_date=timezone.now() + timedelta(days=1),
+        )
+        Task.objects.create_task(
+            task_name='Test Task 1',
+            package_instance=self.pkg,
+            round_=round_,
+            points=5,
+            initialise_task=init_tasks
+        )
+        Task.objects.create_task(
+            task_name='Test Task 2',
+            package_instance=self.pkg,
+            round_=round_,
+            points=5,
+            initialise_task=init_tasks
+        )
+        return round_
 
     def test_06_tasks_listing(self):
         with InCourse(self.course):
-            round1 = Round.objects.create_round(
-                start_date=timezone.now() - timedelta(days=1),
-                deadline_date=timezone.now() + timedelta(days=1),
-            )
-            Task.objects.create_task(
-                name='Test Task',
-                description='Test Description',
-                max_points=5,
-                round=round1,
-            )
-            Task.objects.create_task(
-                name='Test Task 2',
-                description='Test Description 2',
-                max_points=5,
-                round=round1,
-            )
+            round1 = self.generic_round_with_2_tasks()
             self.assertEqual(round1.tasks.count(), 2)
             round1.delete()
 
     def test_07_round_points(self):
         with InCourse(self.course):
-            round1 = Round.objects.create_round(
-                start_date=timezone.now() - timedelta(days=1),
-                deadline_date=timezone.now() + timedelta(days=1),
-            )
-            Task.objects.create_task(
-                name='Test Task',
-                description='Test Description',
-                max_points=5,
-                round=round1,
-            )
-            Task.objects.create_task(
-                name='Test Task 2',
-                description='Test Description 2',
-                max_points=5,
-                round=round1,
-            )
-            self.assertEqual(round1.max_points, 10)
+            round1 = self.generic_round_with_2_tasks()
+            self.assertEqual(round1.round_points, 10)
             round1.delete()
 
 
@@ -233,7 +212,6 @@ class TaskTestMain(TestCase):
     course = None
     round1 = None
     round2 = None
-    creator_03_works = False
 
     @classmethod
     def setUpTestData(cls):
@@ -253,27 +231,27 @@ class TaskTestMain(TestCase):
             Task.objects.all().delete()
 
     def test_01_create_and_delete_task(self):
-        task = create_tasks(self.round1, 1)[0]
+        create_package_task(self.course, self.round1, 'dosko', '1', init_task=False)
         with InCourse(self.course):
             self.assertEqual(Task.objects.count(), 1)
             self.assertEqual(Task.objects.get(
-                name='Test Task 0').first().max_points, 10)
+                task_name='Test task with package').points, 10)
 
     def test_02_create_package_task_no_init(self):
         """
         Creates a task with a package and then checks access to package instance
         """
-        task = create_package_task(self.round1, 'dosko', '1', init_task=False)
+        task = create_package_task(self.course, self.round1, 'dosko', '1', init_task=False)
         with InCourse(self.course):
             self.assertEqual(Task.objects.count(), 1)
             self.assertEqual(task.package_instance.package_source.name, 'dosko')
             self.assertEqual(task.package_instance.commit, '1')
 
     def test_03_create_package_task_init(self):
-        task = create_package_task(self.round1, 'dosko', '1', init_task=True)
+        task = create_package_task(self.course, self.round1, 'dosko', '1', init_task=True)
         with InCourse(self.course):
             pkg = task.package_instance.package
-            self.assertEqual(pkg['name'], 'Liczby Doskonałe')
+            self.assertEqual(pkg['title'], 'Liczby Doskonałe')
             self.assertEqual(len(task.sets), 4)
             self.assertEqual(pkg.sets('set0')['name'], 'set0')
             set0 = task.sets.filter(short_name='set0').first()
@@ -281,22 +259,22 @@ class TaskTestMain(TestCase):
             tests = [t.short_name for t in set0.tests]
             self.assertIn('dosko0a', tests)
             self.assertIn('dosko0b', tests)
-        self.creator_03_works = True
 
     def test_04_init_task_delete_without_results(self):
-        if not self.creator_03_works:
+        try:
+            self.test_03_create_package_task_init()
+        except AssertionError:
             self.skipTest('Creation of package test is not working properly')
+        self.tearDown()
 
-        task = create_package_task(self.round2, 'dosko', '1', init_task=True)
+        task = create_package_task(self.course, self.round2, 'dosko', '1', init_task=True)
         with InCourse(self.course):
             Task.objects.delete_task(task)
-            self.assertEqual(Task.objects.count(), 0)
-            self.assertEqual(TestSet.objects.count(), 0)
-            self.assertEqual(Test.objects.count(), 0)
+            self.assertEqual(Task.objects.count(), 0, f'Tasks: {Task.objects.all()}')
+            self.assertEqual(TestSet.objects.count(), 0, f'TestSets: {TestSet.objects.all()}')
+            self.assertEqual(Test.objects.count(), 0, f'Tests: {Test.objects.all()}')
 
 
-@unittest.skipUnless(TaskTestMain.creator_03_works,
-                     'Creation of package test is not working properly')
 class TestTaskWithResults(TestCase):
     course = None
     round_ = None
@@ -309,8 +287,8 @@ class TestTaskWithResults(TestCase):
             short_name='TC3',
         )
         cls.round_ = create_rounds(cls.course, 1)[0]
-        cls.task1 = create_package_task(cls.round_, 'dosko', '1', init_task=True)
-        cls.task2 = create_package_task(cls.round_, 'dosko', '1', init_task=True)
+        cls.task1 = create_package_task(cls.course, cls.round_, 'dosko', '1', init_task=True)
+        cls.task2 = create_package_task(cls.course, cls.round_, 'dosko', '1', init_task=True)
         cls.user = User.objects.create_user(
             email='test@test.com',
             password='test',
