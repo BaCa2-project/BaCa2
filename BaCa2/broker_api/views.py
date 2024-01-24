24c54420267c61e6783f10ebe72bb3bb7523270d
Bartosz Deptuła <barti.deptula@gmail.com>
2024-01-25 00:38:16 +0100

    MAIN-46 Broker API docs are now up to date. MAIN-41 [UP TO DATE] MAIN-39 [IN PROGRESS]

--- BaCa2/broker_api/views.py
+++ BaCa2/broker_api/views.py
@@ -9,7 +9,25 @@ from baca2PackageManager.broker_communication import *
 
 @csrf_exempt
 def handle_broker_result(request):
-    """Handles result sent by broker."""
+    """
+    Handles the result sent by the broker.
+
+    This function is exempt from CSRF verification. It expects a POST request with a JSON body.
+    The JSON body is parsed and handled by the BrokerSubmit.handle_result method.
+
+    Possible status codes:
+
+    * If the request method is not POST, it returns 404.
+    * If the content type of the request is not 'application/json', it returns 400.
+    * If there is a PermissionError during the processing of the request, it returns 401.
+    * If there is any other exception during the processing of the request, it returns 403.
+    * If the request is processed successfully, it returns 200.
+
+    :param request: The HTTP request received from the broker.
+    :type request: django.http.HttpRequest
+    :return: An HTTP response. The status code of the response depends on the processing of the request.
+    :rtype: django.http.HttpResponse
+    """
     if request.method != 'POST':
         return HttpResponse("Not found", status=404)
 
@@ -29,7 +47,26 @@ def handle_broker_result(request):
 
 @csrf_exempt
 def handle_broker_error(request):
-    """Handles error sent by broker."""
+    """
+    Handles the error sent by the broker.
+
+    This function is exempt from CSRF verification. It expects a POST request with a JSON body.
+    The JSON body is parsed and handled by the BrokerSubmit.handle_error method.
+
+    Possible status codes:
+
+    * If the request method is not POST, it returns 404.
+    * If the content type of the request is not 'application/json', it returns 400.
+    * If there is a PermissionError during the processing of the request, it returns 401.
+    * If there is any other exception during the processing of the request, it returns 403.
+    * If the request is processed successfully, it returns 200.
+
+    :param request: The HTTP request received from the broker.
+    :type request: django.http.HttpRequest
+    :return: An HTTP response. The status code of the response depends on the processing of the request.
+
+    :rtype: django.http.HttpResponse
+    """
     if request.method != 'POST':
         return HttpResponse("Not found", status=404)
 
