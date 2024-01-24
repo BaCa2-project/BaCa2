b3cc4e20ad2d5f68714426f99355e74546173ee5
Bartosz Deptuła <barti.deptula@gmail.com>
2024-01-24 21:01:23 +0100

    MAIN-47 Added course models. After marge need to integrate with course views. MAIN-41 [IN PROGRESS] MAIN-39 [IN PROGRESS]

--- BaCa2/course/models.py
+++ BaCa2/course/models.py
@@ -279,7 +279,7 @@ class TaskManager(models.Manager):
         :type judging_mode: TaskJudgingMode
         :param initialise_task: If True, the task will be initialised using data from
             PackageManager, otherwise sub-objects (tasks, sets & tests) won't be created
-        defaults to True (optional)
+            defaults to True (optional)
         :type initialise_task: bool
         :param course: The course that the task is in, if None - acquired from external definition
             (optional)
@@ -458,7 +458,8 @@ class Task(models.Model, metaclass=ReadCourseMeta):
         :param amount: The amount of submits to return, defaults to 1 (optional)
         :type amount: int
 
-        :return: The last submit of a user for a task.
+        :return: The last submit of a user for a task or a list of 'amount' last submits to that task.
+        :rtype: Submit | List[Submit]
         """
         user = ModelsRegistry.get_user(user)
         self.refresh_user_submits(user)
@@ -476,7 +477,8 @@ class Task(models.Model, metaclass=ReadCourseMeta):
         :param amount: The amount of submits you want to get, defaults to 1 (optional)
         :type amount: int
 
-        :return: The best submit of a user for a task.
+        :return: The best submit of a user for a task or list of 'amount' best submits to that task.
+        :rtype: Submit | List[Submit]
         """
         user = ModelsRegistry.get_user(user)
         self.refresh_user_submits(user)
@@ -493,9 +495,11 @@ class Task(models.Model, metaclass=ReadCourseMeta):
         :param pkg_instance: The PackageInstance object that you want to check for
         :type pkg_instance: PackageInstance
         :param in_every_course: If True, the package instance must be in every course.
-        If False, it must be in at least one course, defaults to True
+            If False, it must be in at least one course, defaults to True
         :type in_every_course: bool (optional)
-        :return: A boolean value.
+
+        :return: True if package instance exists in every course, False otherwise.
+        :rtype: bool
         """
         if not in_every_course:
             return cls.objects.filter(package_instance_id=pkg_instance.pk).exists()
@@ -829,13 +833,13 @@ class Submit(models.Model, metaclass=ReadCourseMeta):
         """
         return list(Result.objects.filter(submit=self).all())
 
-    @transaction.atomic()
+    @transaction.atomic
     def score(self, rejudge: bool = False) -> float:
         """
         It calculates the score of *self* submit.
 
         :param rejudge: If True, the score will be recalculated even if it was already calculated
-        before, defaults to False (optional)
+            before, defaults to False (optional)
         :type rejudge: bool
 
         :return: The score of the submit.
