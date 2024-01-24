bc6786c1a2923845c3a9df7a6898a068e4bd757e
Mateusz Kadula <mateusz.kadula@icloud.com>
2024-01-24 22:19:44 +0100

    Documentation for broker_api app

--- BaCa2/broker_api/scheduler.py
+++ BaCa2/broker_api/scheduler.py
@@ -1,3 +1,5 @@
+"""Contains the scheduler for the broker_api app."""
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.core.management import call_command
 
@@ -9,10 +11,12 @@ scheduler = BackgroundScheduler()
 
 @scheduler.scheduled_job('interval', minutes=BrokerRetryPolicy.retry_check_interval)
 def check_model_updates():
+    """Looks for submits that need to be resent to broker, marks them expired and resends them."""
     call_command('markExpired')
     call_command('resendToBroker')
 
 
 @scheduler.scheduled_job('interval', minutes=BrokerRetryPolicy.deletion_check_interval)
 def delete_old_errors():
+    """Deletes errors that are older than BrokerRetryPolicy.error_deletion_time."""
     call_command('deleteErrors')
