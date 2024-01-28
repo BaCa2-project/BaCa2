673e287ccac1610068384c4d2943dfac3a405436
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2024-01-28 20:33:49 +0100

    Add dict to url encoding and decoding functions for non-nested dicts to util

--- BaCa2/util/other.py
+++ BaCa2/util/other.py
@@ -22,7 +22,7 @@ def normalize_string_to_python(string: str) -> str | bool | int | None:
 
 def add_kwargs_to_url(url: str, kwargs: dict) -> str:
     """
-    Adds the given kwargs to the given url as query parameters.
+    Adds the given kwargs to the given url as query_result parameters.
 
     :param url: The url to add the kwargs to
     :type url: str
@@ -56,3 +56,40 @@ def replace_special_symbols(string: str, replacement: str = '_') -> str:
         if not string[i].isalnum():
             string = string[:i] + f'{replacement}' + string[i + 1:]
     return string
+
+
+def encode_dict_to_url(name: str, dictionary: dict) -> str:
+    """
+    Encodes a dictionary to a string that can be used in a url. Does not support nested
+    dictionaries.
+
+    :param name: The name of the dictionary, the encoded dict can be retrieved from url
+    query parameters using this name.
+    :type name: str
+    :param dictionary: The dictionary to encode.
+    :type dictionary: dict
+    :return: The encoded dictionary.
+    :rtype: str
+
+    See also:
+        - :func:`decode_url_to_dict`
+    """
+    items = '|'.join([f'{key}={value}' for key, value in dictionary.items()])
+    return f'{name}={items}'
+
+
+def decode_url_to_dict(encoded_dict: str) -> dict:
+    """
+    Decodes a string that was encoded using :func:`encode_dict_to_url` to a dictionary normalizing
+    values to python.
+
+    :param encoded_dict: The encoded dictionary.
+    :type encoded_dict: str
+    :return: The decoded dictionary.
+    :rtype: dict
+
+    See also:
+        - :func:`encode_dict_to_url`
+    """
+    items = encoded_dict.split('|')
+    return {item.split('=')[0]: normalize_string_to_python(item.split('=')[1]) for item in items}
