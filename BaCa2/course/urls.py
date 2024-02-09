942b6e146ea712e7529bd38d928cfecf5c2533a7
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2024-02-09 14:01:02 +0100

    move course id url parameter to core.urls declaration

--- BaCa2/course/urls.py
+++ BaCa2/course/urls.py
@@ -5,7 +5,7 @@ from .views import CourseAdmin, CourseView, RoundModelView
 app_name = 'course'
 
 urlpatterns = [
-    path('<int:course_id>/', CourseView.as_view(), name='course-view'),
-    path('<int:course_id>/admin/', CourseAdmin.as_view(), name='course-admin'),
-    path('<int:course_id>/round/', RoundModelView.as_view(), name='round-model-view')
+    path('', CourseView.as_view(), name='course-view'),
+    path('admin/', CourseAdmin.as_view(), name='course-admin'),
+    path('models/round/', RoundModelView.as_view(), name='round-model-view')
 ]
