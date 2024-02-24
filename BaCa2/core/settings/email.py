7915d4ef1414e510b468ed0af03c0829a8eb5692
Bartosz Deptuła <barti.deptula@gmail.com>
2024-02-24 17:06:49 +0100

    Change default "from_mail" to use name (not only email) BWA-29 [DONE] BWA-28 [IN PROGRESS]

--- BaCa2/core/settings/email.py
+++ BaCa2/core/settings/email.py
@@ -6,4 +6,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
-DEFAULT_FROM_EMAIL = 'baca2.noresponse@hotmail.com'
+DEFAULT_FROM_EMAIL = 'BaCa2 mailing system <baca2.noresponse@hotmail.com>'
