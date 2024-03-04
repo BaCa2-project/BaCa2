7d0079933f67838d53faa919252e87f5d215ad17
Bartosz Deptuła <barti.deptula@gmail.com>
2024-03-04 14:56:56 +0100

    Update settings to work properly with oidc auth on server as well as local dev server

--- BaCa2/core/settings/templates/settings.dev.py
+++ BaCa2/core/settings/templates/settings.dev.py
@@ -1,1 +1,11 @@
-DEBUG = False
+DEBUG = True
+
+# Disable SSL redirect
+SECURE_SSL_REDIRECT = False
+
+# Disable oidc authentication
+try:
+    AUTHENTICATION_BACKENDS.remove(  # noqa: F821
+        'mozilla_django_oidc.auth.OIDCAuthenticationBackend')
+except ValueError:
+    pass
