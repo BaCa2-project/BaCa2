e6d5b0b7debcaed21e05b52c09d6c187439ca74b
ZyndramZM <bartosz.deptula@student.uj.edu.pl>
2024-03-07 13:08:37 +0100

    Add docstrings for logout related pages. BWA-39 [DONE] BWA-9 [DONE]

--- BaCa2/core/uj_oidc_auth.py
+++ BaCa2/core/uj_oidc_auth.py
@@ -8,8 +8,20 @@ logger = logging.getLogger(__name__)
 
 
 class BaCa2UJAuth(OIDCAuthenticationBackend):
+    """
+    This class is a custom authentication backend for the BaCa2 application.
+    It extends the OIDCAuthenticationBackend from the mozilla_django_oidc library.
+    """
 
     def filter_users_by_claims(self, claims):
+        """
+        Filters users based on the claims provided.
+
+        :param claims: The claims to filter users by.
+        :type claims: dict
+        :return: QuerySet of User objects that match the claims.
+        :rtype: QuerySet
+        """
         email = claims.get('email')
         if not email:
             return User.objects.none()
@@ -17,6 +29,14 @@ class BaCa2UJAuth(OIDCAuthenticationBackend):
         return User.objects.filter(email=email)
 
     def create_user(self, claims):
+        """
+        Creates a new user based on the claims provided.
+
+        :param claims: The claims to create a user from.
+        :type claims: dict
+        :return: The newly created User object, or None if the user could not be created.
+        :rtype: User or None
+        """
         email = claims.get('email')
         first_name = claims.get('given_name', '')
         last_name = claims.get('family_name', '')
@@ -45,6 +65,16 @@ class BaCa2UJAuth(OIDCAuthenticationBackend):
         return user
 
     def update_user(self, user, claims):
+        """
+        Updates an existing user based on the claims provided.
+
+        :param user: The user to update.
+        :type user: User
+        :param claims: The claims to update the user with.
+        :type claims: dict
+        :return: The updated User object.
+        :rtype: User
+        """
         user.first_name = claims.get('given_name', '')
         user.last_name = claims.get('family_name', '')
 
