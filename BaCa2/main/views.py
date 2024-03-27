0390054a0bfaf2b8e1b4aea075b0ff641d4a37f8
Bartosz Deptuła <barti.deptula@gmail.com>
2024-03-27 13:42:29 +0100

    Development info for students mostly done for now.
    BWA-48 [IN PROGRESS]
    BWA-23 [IN PROGRESS]

--- BaCa2/main/views.py
+++ BaCa2/main/views.py
@@ -484,10 +484,11 @@ class DashboardView(BaCa2LoggedInView):
     See also:
         - :class:`BaCa2LoggedInView`
     """
-    template_name = 'dashboard.html'
+    template_name = 'development_info.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['user_first_name'] = self.request.user.first_name
         return context
 
 
