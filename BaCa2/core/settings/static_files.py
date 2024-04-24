a9ad3baf7d3c7472005496d89b4e2bd6e1f84c86
Bartosz Deptuła <barti.deptula@gmail.com>
2024-04-24 13:32:29 +0200

    scss and css separation (scss shouldnt be static dir) BETA-47 [IN PROGRESS]

--- BaCa2/core/settings/static_files.py
+++ BaCa2/core/settings/static_files.py
@@ -1,11 +1,16 @@
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/zyndram/core/static/'
+STATIC_ROOT = BASE_DIR.parent / 'static'  # noqa: F821
+# MEDIA_ROOT = BASE_DIR.parent / 'media'  # noqa: F821
+# MEDIA_URL = '/data/'
+
+ASSETS_ROOT = BASE_DIR / 'assets'  # noqa: F821
 
 STATICFILES_DIRS = (
-    BASE_DIR / 'assets',  # noqa: F821
+    ASSETS_ROOT / 'static',
     TASK_DESCRIPTIONS_DIR,  # noqa: F821
     SUBMITS_DIR,  # noqa: F821
     ATTACHMENTS_DIR,  # noqa: F821
+    BASE_DIR.parent / 'node_modules',  # noqa: F821
 )
 for d in STATICFILES_DIRS:
     _auto_create_dirs.assert_exists_dir(d)  # noqa: F821
