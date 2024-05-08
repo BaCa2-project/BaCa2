7f4520983990c5e2b37286dfb067e56e23c7765a
Bartosz Deptuła <barti.deptula@gmail.com>
2024-05-08 13:48:06 +0200

    Add possibility to pass packages path, upload tmp path, task description path and attachments path via .env file
    BETA-50 [DONE]
    BETA-22 [IN PROGRESS]

--- BaCa2/core/settings/packages.py
+++ BaCa2/core/settings/packages.py
@@ -1,14 +1,26 @@
+import os
 from typing import Dict
 
 import baca2PackageManager as pkg
 
-PACKAGES_DIR = BASE_DIR / 'packages_source'   # noqa: F821
+PACKAGES_DIR = os.getenv('PACKAGES_DIR')   # noqa: F821
+if not PACKAGES_DIR:
+    PACKAGES_DIR = BASE_DIR / 'packages_source'  # noqa: F821
 _auto_create_dirs.add_dir(PACKAGES_DIR)   # noqa: F821
-UPLOAD_DIR = BASE_DIR / 'uploads'   # noqa: F821
+
+UPLOAD_DIR = os.getenv('UPLOAD_DIR')   # noqa: F821
+if not UPLOAD_DIR:
+    UPLOAD_DIR = BASE_DIR / 'uploads'   # noqa: F821
 _auto_create_dirs.add_dir(UPLOAD_DIR)   # noqa: F821
-TASK_DESCRIPTIONS_DIR = BASE_DIR / 'task_descriptions'   # noqa: F821
+
+TASK_DESCRIPTIONS_DIR = os.getenv('TASK_DESCRIPTIONS_DIR')   # noqa: F821
+if not TASK_DESCRIPTIONS_DIR:
+    TASK_DESCRIPTIONS_DIR = BASE_DIR / 'task_descriptions'   # noqa: F821
 _auto_create_dirs.add_dir(TASK_DESCRIPTIONS_DIR)   # noqa: F821
-ATTACHMENTS_DIR = BASE_DIR / 'attachments'  # noqa: F821
+
+ATTACHMENTS_DIR = os.getenv('ATTACHMENTS_DIR')  # noqa: F821
+if not ATTACHMENTS_DIR:
+    ATTACHMENTS_DIR = BASE_DIR / 'attachments'  # noqa: F821
 _auto_create_dirs.add_dir(ATTACHMENTS_DIR)  # noqa: F821
 
 pkg.set_base_dir(PACKAGES_DIR)
