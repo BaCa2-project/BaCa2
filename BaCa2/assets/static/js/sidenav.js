50681a3104c6813e90d226327acb2360ab9bb6b5
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2024-05-16 16:52:11 +0200

    Update JS logic for new sidenav widget
    BETA-282 [IN PROGRESS]

--- BaCa2/assets/static/js/sidenav.js
+++ BaCa2/assets/static/js/sidenav.js
@@ -1,19 +1,36 @@
 function sidenavSetup() {
     $('.sidenav-tab:not(.has-sub-tabs)').click(function () {
-        const sidenav = $(this).closest('.sidenav');
-        const activeTab = sidenav.find('.sidenav-tab.active');
-        const activatedTab = $(this);
-
-        if (activeTab[0] === activatedTab[0])
-            return;
-
-        const activeContent = $('.tab-content-wrapper.active');
-        const activatedContent = $(`.tab-content-wrapper[data-tab-id=${$(this).attr('id')}]`);
-
-
-        activeTab.removeClass('active');
-        activatedTab.addClass('active').parents('.sidenav-tab').addClass('active');
-        activeContent.trigger('tab-deactivated').removeClass('active').slideUp();
-        activatedContent.trigger('tab-activated').addClass('active').slideDown();
+        sidenavTabClickHandler($(this));
     });
+
+    const tabID = new URL(window.location.href).searchParams.get('tab');
+
+    if (tabID !== null) {
+        const tab = $(`#${tabID}`);
+        if (tab.length > 0)
+            sidenavTabClickHandler(tab);
+    }
+}
+
+function sidenavTabClickHandler(tab) {
+    const sidenav = tab.closest('.sidenav');
+    const activeTab = sidenav.find('.sidenav-tab.active');
+
+    if (activeTab[0] === tab[0])
+        return;
+
+    const activeContent = $('.tab-content-wrapper.active');
+    const activatedContent = $(`.tab-content-wrapper[data-tab-id=${tab.attr('id')}]`);
+
+    activeTab.removeClass('active');
+    tab.addClass('active').parents('.sidenav-tab').addClass('active');
+    activeContent.trigger('tab-deactivated').removeClass('active').slideUp();
+    activatedContent.trigger('tab-activated').addClass('active').slideDown();
+    addTabIDToURL(tab.attr('id'));
+}
+
+function addTabIDToURL(tabID) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('tab', tabID);
+    window.history.replaceState({}, '', url.href);
 }
