6ef10275ec9b9c48be4fd2e31d6635f1e35313e5
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2024-06-05 15:40:47 +0200

    [WIP] Forms JS logic refactor - cleanup
    Remove obsolete functions from forms.js
    
     BETA-21 [IN PROGRESS]

--- BaCa2/assets/static/js/forms.js
+++ BaCa2/assets/static/js/forms.js
@@ -591,56 +591,10 @@ function formsSetup() {
         new FormWidget($(this)).formWidgetInit();
     });
 
-    //ajaxPostSetup();
-    //toggleableGroupSetup();
-    //toggleableFieldSetup();
-    //confirmationPopupSetup();
-    //responsePopupsSetup();
-    //refreshButtonSetup();
-    //selectFieldSetup();
-    //choiceFieldSetup();
     modelChoiceFieldSetup();
     textAreaFieldSetup();
-    //liveValidationSetup();
     tableSelectFieldValidationSetup();
     formObserverSetup();
-}
-
-function ajaxPostSetup() {
-    $('form').filter(function () {
-        return $(this).attr("action") !== undefined;
-    }).on('submit', function (e) {
-        e.preventDefault();
-        handleAjaxSubmit($(this));
-    });
-}
-
-function refreshButtonSetup() {
-    $('form').each(function () {
-        const form = $(this);
-        form.find('.form-refresh-button').on('click', function () {
-            formRefresh(form);
-        });
-    });
-}
-
-function liveValidationSetup() {
-    $('form').each(function () {
-        $(this).find('.live-validation').each(function () {
-            $(this).find('input').filter(function () {
-                return $(this).val() !== undefined && $(this).val().length > 0;
-            }).addClass('is-valid');
-
-            $(this).find('textarea').filter(function () {
-                return $(this).val() !== undefined && $(this).val().length > 0;
-            }).addClass('is-valid');
-
-            $(this).find('select').filter(function () {
-                return $(this).val() !== null && $(this).val().length > 0;
-            }).addClass('is-valid');
-        })
-        submitButtonRefresh($(this));
-    });
 }
 
 function textAreaFieldSetup() {
@@ -649,211 +603,6 @@ function textAreaFieldSetup() {
         const height = `${rows * 2.1}rem`;
         $(this).css('height', height);
     });
-}
-
-// --------------------------------------- toggle setup --------------------------------------- //
-
-function toggleableFieldSetup() {
-    const buttons = $('.field-toggle-btn');
-
-    buttons.each(function () {
-        toggleableFieldButtonInit($(this));
-    });
-
-    buttons.on('click', function (e) {
-        toggleFieldButtonClickHandler(e, $(this));
-    });
-}
-
-function toggleableFieldButtonInit(button) {
-    let on = button.data('initial-state') !== 'off';
-    if (button.hasClass('switch-on') && !on)
-        toggleTextSwitchBtn(button);
-    toggleField(button.closest('.input-group').find('input'), on);
-}
-
-function toggleableGroupSetup() {
-    const buttons = $('.group-toggle-btn');
-
-    buttons.each(function () {
-        toggleableGroupButtonInit($(this));
-    });
-
-    buttons.on('click', function (e) {
-        toggleGroupButtonClickHandler(e, $(this));
-    });
-}
-
-function toggleableGroupButtonInit(button) {
-    let on = button.data('initial-state') !== 'off';
-    if (button.hasClass('switch-on') && !on)
-        toggleTextSwitchBtn(button);
-    toggleFieldGroup(button.closest('.form-element-group'), on);
-}
-
-// ---------------------------------------- popup setup --------------------------------------- //
-
-function confirmationPopupSetup() {
-    const formWrappers = $('.form-wrapper').filter(function () {
-        return $(this).find('.form-confirmation-popup').length > 0;
-    });
-
-    formWrappers.each(function () {
-        const form = $(this).find('form');
-        const popup = $(this).find('.form-confirmation-popup');
-        const submitBtn = form.find('.submit-btn');
-        submitBtn.on('click', function (e) {
-            e.preventDefault();
-            renderConfirmationPopup(popup, form);
-        });
-    });
-
-    $('.form-confirmation-popup .submit-btn').on('click', function () {
-        $(this).closest('.form-confirmation-popup').modal('hide');
-        $('#' + $(this).data('form-target')).submit();
-    });
-}
-
-function responsePopupsSetup() {
-    const forms = $('form').filter(function () {
-        return $(this).data('show-response-popups');
-    });
-
-    forms.on('submit-success', function (e, data) {
-        const popup = $(`#${$(this).data('submit-success-popup')}`);
-        renderResponsePopup(popup, data);
-        popup.modal('show');
-    });
-
-    forms.on('submit-failure', function (e, data) {
-        const popup = $(`#${$(this).data('submit-failure-popup')}`);
-        renderResponsePopup(popup, data);
-        popup.modal('show');
-    });
-}
-
-// ---------------------------------------- ajax submit --------------------------------------- //
-
-function handleAjaxSubmit(form) {
-    const formData = new FormData(form[0]);
-    $.ajax({
-               type: 'POST',
-               url: form.attr('action'),
-               data: formData,
-               contentType: false,
-               processData: false,
-               success: function (data) {
-                   formRefresh(form);
-
-                   form.trigger('submit-complete', [data]);
-
-                   if (data.status === 'success')
-                       form.trigger('submit-success', [data]);
-                   else {
-                       form.trigger('submit-failure', [data]);
-
-                       if (data.status === 'invalid')
-                           form.trigger('submit-invalid', [data]);
-                       else if (data.status === 'impermissible')
-                           form.trigger('submit-impermissible', [data]);
-                       else if (data.status === 'error')
-                           form.trigger('submit-error', [data]);
-                   }
-               }
-           });
-}
-
-// ----------------------------------- field & group toggle ----------------------------------- //
-
-function toggleFieldButtonClickHandler(e, btn) {
-    e.preventDefault();
-    toggleTextSwitchBtn(btn);
-    let on = false;
-    const input = btn.closest('.input-group').find('input');
-
-    if (btn.hasClass('switch-on'))
-        on = true;
-
-    toggleField(input, on);
-
-    if (on)
-        input.focus();
-
-    submitButtonRefresh(btn.closest('form'));
-}
-
-function toggleField(field, on) {
-    if (on)
-        $(field).attr('disabled', false);
-    else {
-        field.val('');
-        if (field.hasClass('is-invalid')) {
-            field.closest('.input-block').find('.invalid-feedback').remove();
-            field.removeClass('is-invalid');
-        }
-        field.removeClass('is-valid');
-        $(field).attr('disabled', true);
-    }
-}
-
-function toggleGroupButtonClickHandler(e, btn) {
-    e.preventDefault();
-    toggleTextSwitchBtn(btn);
-    let on = false;
-    const group = btn.closest('.form-element-group');
-
-    if (btn.hasClass('switch-on'))
-        on = true;
-
-    toggleFieldGroup(group, on);
-
-    if (on)
-        group.find('input:first').focus();
-
-    submitButtonRefresh(btn.closest('form'));
-}
-
-function toggleFieldGroup(formElementGroup, on) {
-    formElementGroup.find('input').each(function () {
-        toggleField($(this), on);
-    });
-}
-
-// --------------------------------------- form refresh --------------------------------------- //
-
-function formRefresh(form) {
-    // form[0].reset();
-    // clearValidation(form);
-    // resetToggleables(form);
-    // submitButtonRefresh(form);
-    // resetHiddenFields(form);
-    new FormWidget(form).resetForm();
-}
-
-function clearValidation(form) {
-    form.find('input').removeClass('is-valid').removeClass('is-invalid');
-    form.find('select').removeClass('is-valid').removeClass('is-invalid');
-    form.find('textarea').removeClass('is-valid').removeClass('is-invalid');
-    form.find('.table-select-field').removeClass('is-valid').removeClass('is-invalid');
-    form.find('.invalid-feedback').remove();
-}
-
-function resetToggleables(form) {
-    form.find('.field-toggle-btn').each(function () {
-        toggleableFieldButtonInit($(this));
-    });
-
-    form.find('.group-toggle-btn').each(function () {
-        toggleableGroupButtonInit($(this));
-    });
-}
-
-function resetHiddenFields(form) {
-    form.find('input[type="hidden"]').each(function () {
-        if ($(this).data('reset-on-refresh') === true)
-            $(this).val('');
-    });
-
 }
 
 // -------------------------------------- live validation ------------------------------------- //
@@ -902,20 +651,6 @@ function updateValidationStatus(field, formCls, formInstanceId, minLength, url) 
                    $(field).trigger('validation-complete');
                }
            });
-}
-
-function updateSelectFieldValidationStatus(field) {
-    if ($(field).val().length === 0) {
-        $(field).removeClass('is-valid');
-        $(field).addClass('is-invalid');
-        $(field).closest('form').find('.submit-btn').attr('disabled', true);
-    } else {
-        $(field).removeClass('is-invalid');
-        $(field).addClass('is-valid');
-        submitButtonRefresh($(field).closest('form'));
-    }
-
-    $(field).trigger('validation-complete');
 }
 
 function submitButtonRefresh(form) {
@@ -1048,64 +783,6 @@ function getClosestTitledElementGroup(field) {
     return field.closest('.form-element-group[data-title]:not([data-title=""])');
 }
 
-// ------------------------------------------ popups ------------------------------------------ //
-
-function renderConfirmationPopup(popup, form) {
-    popup.find('.input-summary-label').text(function () {
-        const inputId = $(this).data('input-target');
-        return getFieldLabel(inputId, form) + ':';
-    });
-
-    popup.find('.input-summary-value').text(function () {
-        const value = $('#' + $(this).data('input-target')).val();
-        return value.length > 0 ? value : '-';
-    });
-}
-
-function renderResponsePopup(popup, data) {
-    const message = popup.find('.popup-message');
-    if (message.data('render-message') === true)
-        message.text(data.message);
-    if (data['status'] === 'invalid')
-        renderValidationErrors(popup, data['errors']);
-    if (data['status'] === 'error')
-        renderErrorMessages(popup, data['errors']);
-}
-
-function renderErrorMessages(popup, errors) {
-    const messageBlock = popup.find('.popup-message-wrapper');
-    const errorsBlock = $('<div class="popup-errors-wrapper text-center"></div>');
-
-    errors.forEach((error) => {
-        errorsBlock.append(`<div class="popup-error mt-2"><b>${error}</b></div>`);
-    });
-
-    messageBlock.after(errorsBlock);
-}
-
-function renderValidationErrors(popup, errors) {
-    const form = popup.closest('.form-wrapper').find('form');
-    const messageBlock = popup.find('.popup-message-wrapper');
-    const errorsBlock = $('<div class="popup-errors-wrapper"></div>');
-
-    Object.entries(errors).forEach(([key, value]) => {
-        const errorDiv = $('<div class="popup-error mt-2"></div>');
-        const fieldLabel = getFieldLabel(key, form);
-
-        errorDiv.append(`<b>${fieldLabel}:</b>`);
-
-        const nestedList = $('<ul class="mb-0"></ul>');
-        value.forEach((nestedValue) => {
-            nestedList.append(`<li>${nestedValue}</li>`);
-        });
-
-        errorDiv.append(nestedList);
-        errorsBlock.append(errorDiv);
-    });
-
-    messageBlock.after(errorsBlock);
-}
-
 // ------------------------------------ table select field ------------------------------------ //
 
 function tableSelectFieldSetup() {
@@ -1177,44 +854,7 @@ function tableSelectFieldCheckboxClickHandler(tableSelectField, input) {
     input.val(ids.join(',')).trigger('input');
 }
 
-// --------------------------------------- select field --------------------------------------- //
-
-function selectFieldSetup() {
-    $('select.auto-width').each(function () {
-        console.log(this);
-        console.log($(this).width());
-        const select = $(this);
-        const tempDiv = $('<div></div>').css({'position': 'absolute', 'visibility': 'hidden'});
-        const tempOption = $('<option class="d-flex"></option>');
-        tempDiv.appendTo('body');
-        tempOption.appendTo(tempDiv);
-        tempOption.text(select.data('placeholder-option'));
-
-        select.find('option').each(function () {
-            tempOption.text($(this).text());
-
-            if (tempOption.width() > select.width())
-                select.width(tempOption.width());
-        });
-
-        tempDiv.remove();
-    });
-}
-
 // ------------------------------------ model choice field ------------------------------------ //
-
-function choiceFieldSetup() {
-    $('.choice-field.placeholder-option').each(function () {
-        const placeholder = $(this).data('placeholder-option');
-        $(this).prepend(`<option class="placeholder" value="" selected>${placeholder}</option>`);
-    });
-
-    $('.choice-field:not(.placeholder-option)').each(function () {
-        const inputBlock = $(this).closest('.input-block');
-        if (inputBlock.find('.live-validation').length > 0)
-            updateSelectFieldValidationStatus($(this));
-    });
-}
 
 function modelChoiceFieldSetup() {
     $('.model-choice-field').each(function () {
